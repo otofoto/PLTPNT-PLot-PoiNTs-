d24a829322b2e61ef7f397189d916e3005ed4a36
Oto <otoluk@gmail.com>
2019-04-16 16:07:08 +0300

    Create pltpntxt.h

---
+++ english/pltpntxt.h
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------+
+|                                                                       |
+|       pltpntxt.h                                                      |
+|                                                                       |
++----------------------------------------------------------------------*/
+#define TXT_DialogTitle         "Plot Points from ASCII File"
+
+#define TXT_LabelColor          "Color"
+#define TXT_LabelWeight         "Weight"
+#define TXT_LabelLevel          "Level"
+#define TXT_LabelXoffset        "Xoffset"
+#define TXT_LabelYoffset        "Yoffset"
+#define TXT_LabelAdjust         "Offset"
+#define TXT_LabelTxtHeight      "Height"
+#define TXT_LabelinRow      	"Label in row"
+
+#define TXT_toggle_Descriptor   " Descriptor:"
+#define TXT_toggle_Label        " Label:"
+#define TXT_toggle_Elev         " Elevation:"
+#define TXT_toggle_Cell         "Cell/°Point:"
+#define TXT_toggle_Lock         "Lock:"
+
+#define TXT_Text                "Text"
+#define TXT_Symbol              "Symbol"
+#define TXT_File                "File"
+
+#define TXT_pBtn_Select         "Select"
+#define TXT_pBtn_Run            "Run"
+#define TXT_pBtn_Cancel         "Cancel"
+
+#define TXT_CellName            "Name:"
+#define TXT_AsciiFile           "File:"
+#define TXT_InputFormat         "Input Format:"
+
+#define TXT_0_DXYZL             "D,X,Y,Z,L"
+#define TXT_1_DYXZL             "D,Y,X,Z,L"
+#define TXT_2_XYZDL             "X,Y,Z,D,L"
+#define TXT_3_YXZDL             "Y,X,Z,D,L"
+#define TXT_4_DXYZL             "D X Y Z L"
+#define TXT_5_DYXZL             "D Y X Z L"
+#define TXT_6_XYZDL             "X Y Z D L"
+#define TXT_7_YXZDL             "Y X Z D L"
+#define TXT_8_BINAER            "B I N A R"
+#define TXT_9_LIDAR             "Lidar_RAW"
+
+#define TXT_SelectFile          "Select ASCII Point File"
+#define TXT_FileNotOpened       "Unable to open point file <%s>"
+#define TXT_CellNotFound        "Unable to activate cell <%s>"
+#define TXT_CellNotPlaced       "Error placing cell <%s> - Status=%d"
+#define TXT_Processing          "Processing line - %-d"
+#define TXT_LinesProcessed      "Processing complete (%d lines processed)"
+#define TXT_Binaerfehler		"Die selectierte Datei ist keine gültige Binärdatei"
+#define TXT_bereich		"Dimension der Zeichnung für die eingestellten Arbeitseinheiten zu groß. Bitte zuerst Arbeitseinheiten umstellen und dann File neu selectieren"
+
+#define TXT_LabelRow         "Label in Row no.:"
+#define TXT_0_ROW             "5"
+#define TXT_1_ROW             "6"
+#define TXT_2_ROW             "6-7"
+#define TXT_3_ROW             "7"
